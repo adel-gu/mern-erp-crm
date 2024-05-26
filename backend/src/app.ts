a7047cc936ff774902dd6f6772f9da1c86168fbc
adel <adeelguitoun@gmail.com>
2024-05-26 17:33:57 +0100

    Add coreAuth Route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,9 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 
+import coreAuthRouter from './routes/coreRoutes/coreAuth';
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
+
+// Routes
+app.use('/api', coreAuthRouter);
 
 export default app;
