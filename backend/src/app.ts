f423e800b5cafce8fe492e442c4f64c1d185a60b
adel <adeelguitoun@gmail.com>
2024-05-27 20:26:23 +0100

    Use cookie parser

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
 
 import coreAuthRouter from './routes/coreRoutes/coreAuth';
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors());
 app.use(morgan('dev'));
 
