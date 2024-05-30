218a2dfacfd45da9dab959971a5755a895b6127d
adel <adeelguitoun@gmail.com>
2024-05-30 19:34:55 +0100

    Remove console.log

--- backend/src/controllers/middlewaresControllers/createCrudController/update.ts
+++ backend/src/controllers/middlewaresControllers/createCrudController/update.ts
@@ -5,12 +5,12 @@ import AppErrorHandler from '../../../handlers/errors/appErrorHandler';
 
 const updateDoc = (model: string) =>
   catchErrors(async (req: Request, res: Response, next: NextFunction) => {
-    console.log('ADMIN: ', req.params.id);
     const Model = mongoose.model(model);
     const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
       runValidators: true,
     });
+
     if (!doc) return next(new AppErrorHandler(`Document not found`, 404));
     res.status(200).json({
       status: 'success',
