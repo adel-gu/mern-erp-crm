fe59d326fb7f693fd9742ce44856302fea2afe5e
adel <adeelguitoun@gmail.com>
2024-05-30 09:34:06 +0100

    Create catch error handler for controllers actions

---
+++ backend/src/handlers/errors/catchErrors.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from 'express';
+
+const catchErrors = (
+  fn: (req: Request, res: Response, next: NextFunction) => Promise<void>,
+) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    fn(req, res, next).catch(next);
+  };
+};
+
+export default catchErrors;
