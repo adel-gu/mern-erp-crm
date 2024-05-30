ed103509af2a2b52aa088c92ec0daeced4cac69f
adel <adeelguitoun@gmail.com>
2024-05-30 11:08:13 +0100

    Handle all auth actions with catchErrors

--- backend/src/controllers/middlewaresControllers/createAuthController/logout.ts
+++ backend/src/controllers/middlewaresControllers/createAuthController/logout.ts
@@ -1,13 +1,9 @@
 import { Request, Response } from 'express';
+import catchErrors from '../../../handlers/errors/catchErrors';
 
-const logout = async (req: Request, res: Response) => {
-  try {
-    res.clearCookie('auth_token');
-    res.sendStatus(200);
-  } catch (error) {
-    console.log('Error: ', error);
-    res.status(500).json({ status: 'error', error });
-  }
-};
+const logout = catchErrors(async (req: Request, res: Response) => {
+  res.clearCookie('auth_token');
+  res.sendStatus(200);
+});
 
 export default logout;
