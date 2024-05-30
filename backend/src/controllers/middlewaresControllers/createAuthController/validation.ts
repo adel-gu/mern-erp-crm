6b2c341e0463b28ac2e3c01930a8668795ad3775
adel <adeelguitoun@gmail.com>
2024-05-30 18:06:20 +0100

    Update error handler controller

--- backend/src/controllers/middlewaresControllers/createAuthController/validation.ts
+++ backend/src/controllers/middlewaresControllers/createAuthController/validation.ts
@@ -7,8 +7,7 @@ const handleValidationErrors = async (
   next: NextFunction,
 ) => {
   const errors = validationResult(req);
-  if (!errors.isEmpty())
-    return res.status(400).json({ errors: errors.array() });
+  if (!errors.isEmpty()) errors.throw;
 
   next();
 };
