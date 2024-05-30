acf237eea5ed15c3962c1bbec89a936ca8e45c6a
adel <adeelguitoun@gmail.com>
2024-05-30 19:33:45 +0100

    Add update to crud controller

--- backend/src/controllers/middlewaresControllers/createCrudController/index.ts
+++ backend/src/controllers/middlewaresControllers/createCrudController/index.ts
@@ -1,14 +1,10 @@
 import readDoc from './read';
 import updateDoc from './update';
 
-// export default {
-//   read: readDoc,
-// };
-
 const createCRUDController = (model: string) => {
   return {
     read: readDoc(model),
-    updateProfile: updateDoc(model),
+    update: updateDoc(model),
   };
 };
 
