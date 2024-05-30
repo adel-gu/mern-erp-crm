27cd597974d84b1115f8afb5753cf96cf1bd3239
adel <adeelguitoun@gmail.com>
2024-05-30 19:37:18 +0100

    Update file to  set the admin id and the req.body

--- backend/src/controllers/middlewaresControllers/createMeController/setUpdateMe.ts
+++ backend/src/controllers/middlewaresControllers/createMeController/setUpdateMe.ts
@@ -1,17 +1,12 @@
 import { Request, Response, NextFunction } from 'express';
-import catchErrors from '../../../handlers/errors/catchErrors';
 import AppErrorHandler from '../../../handlers/errors/appErrorHandler';
 
-const setUpdateMeBody = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
+const setUpdateMe = async (req: Request, res: Response, next: NextFunction) => {
   if (req.body.password || req.body.passwordConfirm)
     return next(
       new AppErrorHandler('This route is not for updating user password', 400),
     );
-
+  req.params.id = req.adminId;
   req.body = {
     name: req.body.name,
     email: req.body.email,
@@ -20,4 +15,4 @@ const setUpdateMeBody = async (
   next();
 };
 
-export default setUpdateMeBody;
+export default setUpdateMe;
