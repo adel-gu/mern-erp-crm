1ad162b302b559e7fd5b5e09254ac2cb14e371dc
adel <adeelguitoun@gmail.com>
2024-06-03 21:12:10 +0100

    Update deleteMe handler

--- backend/src/controllers/user/deleteMe.ts
+++ backend/src/controllers/user/deleteMe.ts
@@ -1,39 +1,17 @@
 import { Request, Response, NextFunction } from 'express';
 import catchErrors from '../../handlers/errors/catchErrors';
-import Admin from '../../models/coreModels/Admin';
-import AdminPassword from '../../models/coreModels/AdminPassword';
-import mongoose from 'mongoose';
+import Admin from '../../models/admin';
 
 const deleteMe = catchErrors(
   async (req: Request, res: Response, next: NextFunction) => {
-    const session = await mongoose.startSession();
-    session.startTransaction();
-    try {
-      const admin = await Admin.findByIdAndUpdate(
-        req.adminId,
-        {
-          active: false,
-        },
-        { session },
-      );
-      const adminPassword = await AdminPassword.findOneAndUpdate(
-        {
-          user: req.adminId,
-        },
-        { active: false },
-        { session },
-      );
+    await Admin.findByIdAndUpdate(req.params.id, {
+      active: false,
+    });
 
-      // commit transaction
-      await session.commitTransaction();
-      session.endSession();
-
-      res.status(204).json({ status: 'Success', data: null });
-    } catch (error) {
-      await session.abortTransaction();
-      session.endSession();
-      throw error;
-    }
+    res
+      .status(204)
+      .clearCookie('auth_token')
+      .json({ status: 'Success', data: null });
   },
 );
 
