e21126e9c1810ffc26860974daaf9579d8997d15
adel <adeelguitoun@gmail.com>
2024-06-04 13:59:43 +0100

    Update crud controller: add all the handlers

--- backend/src/controllers/crud/index.ts
+++ backend/src/controllers/crud/index.ts
@@ -1,10 +1,16 @@
+import readAllDocs from './readAll';
 import readDoc from './read';
+import createDoc from './create';
 import updateDoc from './update';
+import deleteDoc from './delete';
 
 const createCRUDController = (model: string) => {
   return {
+    readAll: readAllDocs(model),
     read: readDoc(model),
+    create: createDoc(model),
     update: updateDoc(model),
+    delete: deleteDoc(model),
   };
 };
 
