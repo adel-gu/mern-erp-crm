de112498bc5f4ec4d1f16b236159925874022228
adel <adeelguitoun@gmail.com>
2024-06-06 13:24:10 +0100

    Add client route

--- backend/src/routes/appApi.ts
+++ backend/src/routes/appApi.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import people from '../controllers/app/people';
 import company from '../controllers/app/company';
+import client from '../controllers/app/client';
+
 import setupRoute from '../utils/setupAppRoutes';
 import { Resource } from '../utils/Constants';
 
@@ -9,5 +11,6 @@ const router = express.Router();
 //                  !App resource                    //
 setupRoute(router, Resource.people, people);
 setupRoute(router, Resource.company, company);
+setupRoute(router, Resource.client, client);
 
 export default router;
